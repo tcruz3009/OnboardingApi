9490902a1f78bd7476525f2a79a7aa0d2ef2a306
Tiago Cruz <t.cruz@totvs.com.br>
2024-05-13 20:16:37 -0300

    add custom swagger documentations

--- OnboardingApi/Program.cs
+++ OnboardingApi/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCustomSwagger();
 
 builder.Services.AddMemoryCache();
 
